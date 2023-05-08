cca63c1b5748a368497a6ad47126ae2bb9b06190
Vivek <tailorvivekgec19@gmail.com>
2023-05-08 15:42:12 +0530

    beat box

--- lib/screens/select_home/beat_home.dart
+++ lib/screens/select_home/beat_home.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:music_beat/screens/select_playlist/beat_playlist.dart';
+import 'package:music_beat/screens/select_setting/beat_setting.dart';
 
 class Beat_Home extends StatefulWidget {
   const Beat_Home({Key? key}) : super(key: key);
@@ -14,30 +16,45 @@ class _Beat_HomeState extends State<Beat_Home> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          title: Text("Beat Box",style: TextStyle(fontSize: 25),),
-          actions: [
-            Padding(
-             padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Icon(Icons.search_rounded,size: 30,),
-              ),
-          ],
+      child: DefaultTabController(
+        initialIndex: 1,
+        length: 5,
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            title: Text("Beat Box",style: TextStyle(fontSize: 25),),
+            actions: [
+              Padding(
+               padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Icon(Icons.search_rounded,size: 30,),
+                ),
+            ],
 
-          bottom: TabBar(
-            isScrollable: true,
-            unselectedLabelColor: Colors.white70,
-            unselectedLabelStyle: TextStyle(fontSize: 15),
-            labelStyle: TextStyle(fontSize: 18),
-            labelColor: Colors.orange,
-            tabs: [
-            Tab(text: "Suggested",),
-            Tab(text: "Songs",),
-            Tab(text: "Artists",),
-            Tab(text: "Albums",),
-            Tab(text: "Folders",),
-          ],),
+            bottom: TabBar(
+              isScrollable: true,
+              unselectedLabelColor: Colors.white70,
+              unselectedLabelStyle: TextStyle(fontSize: 15),
+              labelStyle: TextStyle(fontSize: 18),
+              labelColor: Colors.orange,
+              tabs: [
+              Tab(text: "Suggested",),
+              Tab(text: "Songs",),
+              Tab(text: "Artists",),
+              Tab(text: "Albums",),
+              Tab(text: "Folders",),
+            ],),
+          ),
+
+          body: TabBarView(
+            children: [
+              Beat_Playlist(),
+              Beat_Playlist(),
+              Beat_Setting(),
+              Beat_Playlist(),
+              Beat_Setting()
+
+            ],
+          ),
         ),
       ),
     );
